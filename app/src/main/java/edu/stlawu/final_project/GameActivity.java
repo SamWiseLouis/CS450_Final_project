47f931570ecc164376d4505e745fa12c63b1500e
SamWiseLouis <slemer15@stlawu.edu>
2018-10-29 17:11:33 -0400

    updated the movement of the ball to be more smooth with online help, now it also bounces off the walls in a way that decreases its own velocity and reacts in a more marble like fashion instead of a bullet of jitters

--- app/src/main/java/edu/stlawu/final_project/GameActivity.java
+++ app/src/main/java/edu/stlawu/final_project/GameActivity.java
@@ -1,28 +1,34 @@
 package edu.stlawu.final_project;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class GameActivity  extends Activity implements SensorEventListener
-{
-    /** Called when the activity is first created. */
+
+public class GameActivity extends Activity implements SensorEventListener{
+
     CustomDrawableView mCustomDrawableView = null;
     ShapeDrawable mDrawable = new ShapeDrawable();
-    public static int x;
-    public static int y;
-
+    public float xPosition, xAcceleration,xVelocity = 0.0f;
+    public float yPosition, yAcceleration,yVelocity = 0.0f;
+    public float xmax,ymax;
+    private Bitmap mBitmap;
+    private Bitmap mWood;
     private SensorManager sensorManager = null;
+    public float frameTime = 0.666f;
 
     /** Called when the activity is first created. */
     @Override
@@ -30,48 +36,86 @@ public class GameActivity  extends Activity implements SensorEventListener
     {
 
         super.onCreate(savedInstanceState);
+
+
+
+
         // Get a reference to a SensorManager
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+
         mCustomDrawableView = new CustomDrawableView(this);
         setContentView(mCustomDrawableView);
         // setContentView(R.layout.main);
 
+        //set the boundry of the balls movement
+        Display display = getWindowManager().getDefaultDisplay();
+        xmax = (float)display.getWidth() - 100;
+        ymax = (float)display.getHeight() - 100;
     }
 
-    // This method will update the UI on new sensor events
+    // the equivalent of a sensor event listener
     public void onSensorChanged(SensorEvent sensorEvent)
     {
-        {
-            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                // the values you were calculating originally here were over 10000!
-                x = (int) Math.pow(sensorEvent.values[1], 2);
-                y = (int) Math.pow(sensorEvent.values[2], 2);
-
-            }
-
+        {   // for some reason this works the best but the magnetic force one is reliable too
             if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
-
+                //Set sensor values as acceleration
+                yAcceleration = sensorEvent.values[1];
+                xAcceleration = sensorEvent.values[2];
+                updateBall();
             }
         }
     }
+    //stuff to update the balls speed location and such
+    private void updateBall() {
 
+        //Calculate new speed
+        xVelocity += (xAcceleration * frameTime)/2;
+        yVelocity += (yAcceleration * frameTime)/2;
+
+        //Calc distance travelled in that time
+        float xS = (xVelocity/4)*frameTime;
+        float yS = (yVelocity/4)*frameTime;
+
+        //sensors are opposite so this changes them to act accordingly to what we want
+        xPosition -= xS;
+        yPosition -= yS;
+
+        //need to make a deadzone so that the ball is not so touchy
+        // and the player can have better controls
+
+        // ball will bounce off sides and invert velocity direction
+        // also decreses the velocity so that the ball acts more like a marble and less like
+        // a bouncy ball
+        if (xPosition > xmax) {
+            xPosition = xmax;
+            xVelocity = xVelocity*-1/2;
+        } else if (xPosition < 0) {
+            xPosition = 0;
+            xVelocity = xVelocity*-1/2;
+        }
+        if (yPosition > ymax) {
+            yPosition = ymax;
+            yVelocity = yVelocity*-1/2;
+        } else if (yPosition < 0) {
+            yPosition = 0;
+            yVelocity = yVelocity*-1/2;
+        }
+    }
+    // diffrence in power saving mode vs normal?
     // I've chosen to not implement this method
     public void onAccuracyChanged(Sensor arg0, int arg1)
     {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        // Register this class as a listener for the accelerometer sensor
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
-        // ...and the orientation sensor
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -82,28 +126,34 @@ public class GameActivity  extends Activity implements SensorEventListener
         super.onStop();
     }
 
+
+    //drawing the screen and the ball from a resource image
     public class CustomDrawableView extends View
     {
-        static final int width = 50;
-        static final int height = 50;
 
         public CustomDrawableView(Context context)
         {
             super(context);
+            Bitmap ball = BitmapFactory.decodeResource(getResources(), R.drawable.one);
+            final int dstWidth = 100;
+            final int dstHeight = 100;
+            mBitmap = Bitmap.createScaledBitmap(ball, dstWidth, dstHeight, true);
 
-            mDrawable = new ShapeDrawable(new OvalShape());
-            mDrawable.getPaint().setColor(0xff74AC23);
-            mDrawable.setBounds(x, y, x + width, y + height);
+
         }
-
+        // the canvas that these objects are being drawn on
         protected void onDraw(Canvas canvas)
         {
-            RectF oval = new RectF(GameActivity.x, GameActivity.y, GameActivity.x + width, GameActivity.y
-                    + height); // set bounds of rectangle
-            Paint p = new Paint(); // set some paint options
-            p.setColor(Color.BLUE);
-            canvas.drawOval(oval, p);
+            final Bitmap bitmap = mBitmap;
+            canvas.drawBitmap(bitmap, xPosition, yPosition, null);
             invalidate();
         }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // TODO Auto-generated method stub
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
